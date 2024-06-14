8d391de421c0538bf232ebf1308089ad00c6fa54
Rido-Putra <ridofku@gmail.com>
2024-06-14 23:39:38 +0700

    update 14June2024

---
+++ exercise.py
@@ -0,0 +1,23 @@
+for animal in ["dog", "cat", "mouse"]:
+    print("{} is a mammal".format(animal))
+
+
+for number in [1, 2, 3, 4]:
+    print("{}is number".format(number))
+for animal in ["dog", "cat", "jerry"]:
+    print("{} is a mammal".format(animal))
+
+v = "test"
+dict = {"name": "reno", "class": 10, "hobby": "running", "country": "Indonesia"}
+
+long_dict = {
+    "key1": "value1",
+    "key2": "value2",
+    "key3": "value3",
+    "key100": "value100",
+    "key101": "value101",
+    "key102": "value102",
+    "key103": "value103",
+    "key104": "value104",
+    "key105": "value105",
+}
--- main.py
+++ main.py
@@ -3,14 +3,18 @@ from bs4 import BeautifulSoup
 import re
 import sqlite3
 
+
 def opening():
+    """Display welcome message."""
     print("------------------------------------------")
     print("Hello, Welcome to scrapping project!")
     print("------------------------------------------")
-    return 
+    return
 
-#HTTP Request
-def get_request(url):  
+
+# HTTP Request
+def get_request(url):
+    """Make an  HTTP request to the given URL  and return the HTML content if successful"""
     request = requests.get(url)
     if request.status_code == 200:
         print("request succesful")
@@ -19,53 +23,66 @@ def get_request(url):
     else:
         print("request failed")
 
-#HTML Parsing
+
+# HTML Parsing
 def get_soup(html):
+    """Parse HTML content using BeautifulSoup and return the soup objeck"""
     soup = BeautifulSoup(html, "html.parser")
     return soup
 
-#Regular Expression --> cheat untuk ambil data by pola
+
+# Regular Expression --> cheat untuk ambil data by pola
 def regexpression(string):
-    pattern = r'\((\d+) available\)'
+    """Extract availability number using regular expression."""
+    pattern = r"\((\d+) available\)"
     matches = re.search(pattern, string)
     if matches:
         availability = matches.group(1)
         return availability
-    else: 
+    else:
         return None
-    
+
+
 # Scrape One Product
 def scrape_product(url):
+    """Scrape product details from given URL."""
     html = get_request(url)
     if html is None:
         return None
-    
+
     soup = get_soup(html)
     title = soup.find("h1").get_text()
-    price = soup.find("p", class_= "price_color").get_text()
+    price = soup.find("p", class_="price_color").get_text()
     price_fix = float(price.replace("£", ""))
-    stock_availability = regexpression(soup.find("p", class_ ="instock availability").get_text())
-    description = soup.find("div", id ="content_inner").find_all("p")
+    stock_availability = regexpression(
+        soup.find("p", class_="instock availability").get_text()
+    )
+    description = soup.find("div", id="content_inner").find_all("p")
     # for index , p in enumerate(decsription): ==> find index "p"
     #     print(f'index {index}, p {p}')
     final_description = description[3]
-    final_description = final_description.get_text()        
-    product = {"title" : title, 
-               "price" : price_fix, 
-               "stock availability" : stock_availability, 
-               "product description" : final_description }  
+    final_description = final_description.get_text()
+    product = {
+        "title": title,
+        "price": price_fix,
+        "stock availability": stock_availability,
+        "product description": final_description,
+    }
     return product
 
-#extract product info page 1 from soup
+
+# extract product info page 1 from soup
 def product_detail(soup):
-    products:list[BeautifulSoup] = soup.find_all("li", class_ ="col-xs-6 col-sm-4 col-md-3 col-lg-3")
+    products: list[BeautifulSoup] = soup.find_all(
+        "li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3"
+    )
     urls = []
     for index, i in enumerate(products):
         relative_url = i.find("a")["href"]
         full_url = f"https://books.toscrape.com/{relative_url}"
         urls.append(full_url)
 
-    product_infos:list[BeautifulSoup] = []
+    product_infos: list[BeautifulSoup] = []
     for index, url in enumerate(urls):
         product_info = scrape_product(url)
         if product_info:
@@ -73,6 +90,39 @@ def product_detail(soup):
             product_infos.append(product_info)
     return product_infos
 
+
+def save_to_sqlite(data):
+    # membuat koneksi ke database(atau mebuat database jika belum ada)
+    conn = sqlite3.connect("products.db")
+    cursor = conn.cursor()
+
+    # membuat tabel jika belum ada
+    cursor.execute(
+        """
+                      CREATE TABLE IF NOT EXISTS products (
+                      id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      title TEXT,
+                      price REAL,
+                      stock_availability INTEGER,
+                      product_description TExt)"""
+    )
+
+    # memasukan data dari dictionary ke dalam tabel
+    for product in data:
+        cursor.execute(
+            """INSERT INTO products (title, price, stock_availability, product_description)
+                          VALUES (?, ?, ?, ?)""",
+            (
+                product["title"],
+                product["price"],
+                product["stock availability"],
+                product["product description"],
+            ),
+        )
+
+    # menyimpan perubahan dan menutup koneksi
+    conn.commit()
+    conn.close()
 
 
 if __name__ == "__main__":
@@ -85,55 +135,33 @@ if __name__ == "__main__":
     5. construct dictionary info one variable"""
 
     welcome = opening()
-    
-    base_url = "https://books.toscrape.com/index.html"
-    html = get_request(base_url)
-    soup = get_soup(html)
-    products = product_detail(soup)
-    print(products)
-    for index, product in enumerate(products):
-        print(index, product)
 
-    # products = [{"title" : "Book 1",
-    #             "price" : 10.23,
-    #             "stock_availability" : 20,
-    #             "product_description" : "Describe of Book 1"},
-    #             {"title" : "Book 2",
-    #             "price" : 20.23,
-    #             "stock_availability" : 5,
-    #             "product_description" : "Describe of Book 2"},
-    #             {"title" : "Book 3",
-    #             "price" : 30.23,
-    #             "stock_availability" : 2,
-    #             "product_description" : "Describe of Book 3"
-    #             }]
+    base_url = "https://books.toscrape.com/"
+    url = "https://books.toscrape.com/catalogue/page-2.html"
+    # html = get_request(base_url)
+    # if html:
+    #     soup = get_soup(html)
+    # products = product_detail(soup)
+    # for index, product in enumerate(products):
+    #     print(index+1, product)
+    # save_to_sqlite(products)
 
-    def save_to_sqlite(data):
-        #membuat koneksi ke database(atau mebuat database jika belum ada)
-        conn = sqlite3.connect("products.db")
-        cursor = conn.cursor()
-
-        #membuat tabel jika belum ada
-        cursor.execute("""
-                      CREATE TABLE IF NOT EXISTS products (
-                      id INTEGER PRIMARY KEY AUTOINCREMENT,
-                      title TEXT,
-                      price REAL,
-                      stock_availability INTEGER,
-                      product_description TExt)""")
-        
-        #memasukan data dari dictionary ke dalam tabel
-        for product in data:
-            cursor.execute('''INSERT INTO products (title, price, stock_availability, product_description)
-                          VALUES (?, ?, ?, ?)'''
-                           , (product["title"],product["price"], product["stock availability"], product["product description"]))
-        
-        #menyimpan perubahan dan menutup koneksi
-        conn.commit()
-        conn.close()
-
-    save_to_sqlite(products)
-
-    
-    
-
+    # def scrape_multiple_pages(base_url, num_pages):
+    #     """Scrape multiple pages starting from the base URL."""
+    all_products = []
+    for page_num in range(1, 2):
+        url = f"{base_url}catalogue/page-{page_num}.html"
+        html = get_request(url)
+        if html:
+            soup = get_soup(html)
+            products = product_detail(soup)
+            # all_products.extend(products)
+        else:
+            break
+        # return all_products
+    # num_pages = 1
+    # products = scrape_multiple_pages(base_url, num_pages)
+    # # print(products)
+    # for index, product in enumerate(products):
+    #     print(index, product)
+    # all_product = []
